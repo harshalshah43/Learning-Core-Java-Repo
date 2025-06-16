16b1887de032b5c8f1ff010797fb52aa483a47d7
Harshal Shah <65274384+harshalshah43@users.noreply.github.com>
2025-06-16 12:14:07 +0530

    Arrays Intro
    Arrays Intro

---
+++ arrays1.java
@@ -0,0 +1,40 @@
+//introduction
+import java.util.*;
+
+class arrays1
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter Number of Elements");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+
+        for(int i = 0;i < n; i++)
+        {
+            a[i] = sc.nextInt();
+        }    
+        
+        System.out.println("--------");
+
+        for(int i = 0;i <= n -1; i++)  
+        {
+            System.out.println(a[i]);
+        }    
+
+        System.out.println("Lets work with char array, Enter the value of n");
+
+        n = sc.nextInt();
+        sc.nextLine();
+        char c[] = new char[n];
+        c = sc.nextLine().toCharArray();
+
+        System.out.println(c);
+
+        for(int i = 0;i <= n - 1; i++)  
+        {
+            System.out.println(c[i]);
+        } 
+        
+    }
+}
