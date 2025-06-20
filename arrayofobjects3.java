06f640c71755a513b93641a442819cc7c8547d48
Harshal Shah <65274384+harshalshah43@users.noreply.github.com>
2025-06-20 12:18:22 +0530

    `
    Assignment Array of objects solution

---
+++ arrayofobjects3.java
@@ -0,0 +1,76 @@
+import java.util.*;
+class Student
+{
+    private String name;
+    private double age;
+
+    public Student(String name, double age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void show_student()
+    {
+        System.out.println("Name " + name + "Age" + age);
+    }
+
+    public double getAge()
+    {
+        return age;
+    }
+}
+
+class AgeCalculator
+{   
+    public Student[] getStudentDetails(Student s[])
+    {
+        // Taking student details from user
+        Scanner sc = new Scanner(System.in);
+        String name;
+        double age;
+        for(int i= 0;i < s.length;i++)
+        {   
+            name = sc.nextLine();
+            age = sc.nextDouble();
+            sc.nextLine();
+            s[i] = new Student(name,age);                    
+        }
+
+        return s;
+    }
+    public void calculateAge(Student s[])
+    {   
+        // Calulate Average Age of Students in a classroom
+        
+        double sum = 0;
+        for(int i = 0; i < s.length; i++)
+        {
+            sum += s[i].getAge();
+        }
+        double average = sum/s.length;
+        
+        System.out.println("Average age of the classroom is :" + average);
+    }
+}
+
+class arrayofobjects3
+{
+    public static void main(String[] args)
+    {       
+        int n = 5;
+        Student s[] = new Student[n]; // s is an array of objects
+        
+        // s[0] = new Student("ABC",23);
+        // s[1] = new Student("PQR",25);
+        // s[2] = new Student("XYZ",26);
+        // s[3] = new Student("DCE",27);
+        // s[4] = new Student("DEF",28);
+
+        AgeCalculator a = new AgeCalculator(); //Single object
+        s = a.getStudentDetails(s);
+        a.calculateAge(s);
+
+        
+    }
+}
