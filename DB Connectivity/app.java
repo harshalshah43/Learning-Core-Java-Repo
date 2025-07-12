6aa8ee2883adfb6fa2de0827650598c3084b89d9
Harshal Shah <65274384+harshalshah43@users.noreply.github.com>
2025-07-12 12:07:51 +0530

    Reordering folders

--- DB Connectivity/app.java
+++ DB Connectivity/app.java
@@ -4,6 +4,5 @@ public class app {
     public static void main(String[] args) {
         DisplayRecords d = new DisplayRecords("Hi How are you"); // default constructor
         Connection conn = d.getConnection();
-        d.get_data(conn);
     }
 }
